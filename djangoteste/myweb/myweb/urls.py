016eb0dc3acc47aa4ed93dde338aebc4c5f3ee9d
ajitaravind231 <ajithpa@protonmail.com>
2022-09-02 20:21:07 +0530

    templates

--- djangoteste/myweb/myweb/urls.py
+++ djangoteste/myweb/myweb/urls.py
@@ -1,7 +1,7 @@
-"""myweb URL Configuration
+"""new1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("polls/",include ('polls.urls'))
+    path('polls/',include('polls.urls')),
 ]
--- djangoteste/myweb/polls/views.py
+++ djangoteste/myweb/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
 def home(request):
-    return HttpResponse("test_for_index") 
+    return HttpResponse("hello world")
+
+# Create your views here.
