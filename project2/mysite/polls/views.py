296a2c48456249535d9e0eb4eb49b6b3910c5268
ajitaravind231 <ajithpa@protonmail.com>
2022-09-18 18:15:54 +0530

    404

--- project2/mysite/polls/views.py
+++ project2/mysite/polls/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import  render
 from django.http import HttpResponse
+from django.template import loader
 
-def home(request):
-    return render(request,"test.html",{ "name" : "achu" } )
+from .models import Question
+
+#def home(request):
+    #return render(request,"test.html",{ "name" : "achu" } )
+
+def detail(request, question_id):
+    #return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 
 # Create your views here.
