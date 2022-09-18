9febb64b6c1de24fa953da3607c56672c9a6f37c
ajitaravind231 <ajithpa@protonmail.com>
2022-09-18 19:18:16 +0530

    redirects 2

--- project2/mysite/polls/views.py
+++ project2/mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import  render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
 from .models import Question,Choice
 
@@ -21,8 +22,7 @@ def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
+    except (KeyError, Choice.DoesNotExist):  
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -30,8 +30,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return 
-        HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 def index(request):
