17162e8b1c0130f5946c96e9778473bf802bbfeb
ajitaravind231 <ajithpa@protonmail.com>
2022-09-21 22:36:43 +0530

    model tests

--- project2/mysite/mysite/settings.py
+++ project2/mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
+    'tt1.apps.Tt1Config',
 ]
 
 MIDDLEWARE = [
--- project2/mysite/tt1/models.py
+++ project2/mysite/tt1/models.py
@@ -1,3 +1,19 @@
+import datetime
+from pyexpat import model
+from sre_constants import MAX_REPEAT
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class place(models.Model):
+    name = models.CharField(max_length=20,null=print("Error"),primary_key=True)
+    phone_no=models.IntegerField();
+
+class manager(models.Model):
+    work_place=models.ForeignKey(place, on_delete=models.CASCADE)
+    mgr_name=models.CharField(max_length=20)
+    mgr_dob=models.CharField(max_length=8)
+
+
+
+
