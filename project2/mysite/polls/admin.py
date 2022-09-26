70147b11e55a4f38b40f14a047bee9e6992cbe5a
ajitaravind231 <ajithpa@protonmail.com>
2022-09-26 15:22:50 +0530

    bckp

--- project2/mysite/polls/admin.py
+++ project2/mysite/polls/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from project2.mysite.tt1.models import Place
+#from project2.mysite.tt1.models import Place
 from .models import Question
 
 admin.site.register(Question)
-admin.site.register(Place)
 # Register your models here.
--- project2/mysite/tt1/admin.py
+++ project2/mysite/tt1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Place
+
+admin.site.register(Place),
+
 # Register your models here.
