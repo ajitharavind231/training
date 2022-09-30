75ef4f6809db9128e00be40b205ff693626e4250
ajitaravind231 <ajithpa@protonmail.com>
2022-09-30 18:04:36 +0530

    ignore test

--- project2/mysite/tt1/urls.py
+++ project2/mysite/tt1/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name="polls"
 urlpatterns=[
-    path("",views.home,name="home")
+    path("",views.home,name="home"),
+    path('<str:name>/', views.work_place, name='workplace') 
 ]
 
+ 
--- project2/mysite/tt1/views.py
+++ project2/mysite/tt1/views.py
@@ -1,8 +1,16 @@
+from unicodedata import name
 from django.http import HttpResponse
-from django.shortcuts import render
+from urllib import request
+from django.shortcuts import get_object_or_404, render
+from .models import Place,Manager
 
 # Create your views here.
 def home(request):
     return render(request,"home.html")
     #return HttpResponse("Hello, world. You're at the polls index.")
+
+def work_place(request,name):
+    placename= get_object_or_404(Place,pk=name)
+    return HttpResponse("THIS IS THE WORK PLACE PAGE")
+    
 
